76572497717dd00c21f138ee913c2851838752ce
jewel1992 <37346657+jewel1992@users.noreply.github.com>
2023-10-17 11:35:37 -0400

    Add files via upload

---
+++ landing page/index.js
@@ -0,0 +1,6 @@
+function myBuybutton() {
+    window.open("https://www.amazon.com/stores/page/EF33F7D4-A35F-42D3-8B5B-3D6B24D3D113?almBrandId=VUZHIFdob2xlIEZvb2Rz&ingress=0&visitId=9acf1c9c-71bc-48a9-b722-08deed7e92c3&lp_slot=auto-sparkle-hsa-tetris&i=wholefoods&store_ref=SB_A08044461QDRSC6BOTODU&fpw=alm&ref_=sbx_be_s_sparkle_lsscd_logo");
+
+  }
+
+ 
